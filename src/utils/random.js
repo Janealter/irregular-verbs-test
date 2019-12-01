5866548cdcd52baa1d09a8f5763d1445bdf2105c
Janealter <janealter_92@mail.ru>
2019-12-01 14:44:43 +0300

    random functions: increased default max value

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,12 +1,13 @@
-export function getTwoRandomNumbers (max = 1) {
+export function getTwoRandomNumbers (max = 2) {
   const firstNumber = getRandomNumber(max);
   let secondNumber;
   do {
     secondNumber = getRandomNumber(max);
-  } while (secondNumber === firstNumber)
+  } while (secondNumber === firstNumber);
   return [firstNumber, secondNumber];
 }
 
-export function getRandomNumber (max = 1) {
+// min = 1
+export function getRandomNumber (max = 2) {
   return Math.floor(Math.random() * max + 1);
 }
